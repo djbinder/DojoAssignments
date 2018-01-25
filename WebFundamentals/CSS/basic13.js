96fc4e4a1328291fdba3e8fb2f39fd8a19e7eba2
Dan Binder <djbinder@gmail.com>
2018-01-25 11:07:50 -0600

    Added DojoAssignments Repository
    Changed primary repository for Coding Dojo assignments to main CodingAssignments folder on Google Driver

---
+++ WebFundamentals/CSS/basic13.js
@@ -0,0 +1,10 @@
+function print()
+{
+    var num = 1;
+    while (num <= 255) {
+        console.log(num);
+        num = num + 1; 
+    }
+}
+
+print()
---
+++ WebFundamentals/CSS/basic_13.js
@@ -0,0 +1,7 @@
+function print1to255(){
+        var num = 1;
+        while (num <= 255) {
+            console.log(num);
+            num = num + 1;
+        }
+    }
